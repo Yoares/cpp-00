5c8bc69e4d4acee240133da1aab55d925492ef07
Yasser Khoussi <ykhoussi@e1r1p5.1337.ma>
2025-11-01 19:39:28 +0100

    Account

---
+++ ex02/Account.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykhoussi <ykhoussi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/01 18:13:05 by ykhoussi          #+#    #+#             */
+/*   Updated: 2025/11/01 18:21:57 by ykhoussi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Account.hpp>
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+void Account::_displayTimestamp() {
+    // Format: [YYYYMMDD_HHMMSS]
+    // Use std::time, std::localtime, std::strftime
+    // Output to std::cout
+
+    
+}
