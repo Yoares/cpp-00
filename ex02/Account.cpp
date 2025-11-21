61245358e73e1d9d26528afbf8b5dd9d6489ca2c
Yasser Khoussi <ykhoussi@c4r1p1.1337.ma>
2025-11-21 22:57:29 +0100

    Refactor Account class for improved readability and maintainability

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: ykhoussi <ykhoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 18:13:05 by ykhoussi          #+#    #+#             */
-/*   Updated: 2025/11/21 21:05:45 by ykhoussi         ###   ########.fr       */
+/*   Updated: 2025/11/21 22:47:11 by ykhoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,59 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-void Account::_displayTimestamp() {
-    // Format: [YYYYMMDD_HHMMSS]
-    // Use std::time, std::localtime, std::strftime
-    // Output to std::cout
-    std::time_t now = std::time(NULL);
-    std::tm *ptr = std::localtime(&now);
+void Account::_displayTimestamp(void)
+{
+    std::time_t now = std::time(NULL);        // Get current time in seconds
+    std::tm *ptm = std::localtime(&now);      // Convert to readable time struct
 
-    std::cout << "[";
-    std:: cout << ptr->tm_hour;
-    std:: cout << ptr->tm_min
-    
+    std::cout << '['
+              << (ptm->tm_year + 1900)        // year since 1900 → real year
+              << std::setfill('0') << std::setw(2) << ptm->tm_mon + 1  
+              << std::setw(2) << ptm->tm_mday   // day
+              << '_'
+              << std::setw(2) << ptm->tm_hour   
+              << std::setw(2) << ptm->tm_min   
+              << std::setw(2) << ptm->tm_sec   
+              << "] ";
+}
+
+Account::Account(int initial_deposit)
+{
+    this->_accountIndex = Account::_nbAccounts;
+	Account::_nbAccounts++;
+
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	Account::_totalAmount += initial_deposit; // Add to global money
+
+	_displayTimestamp();
+	// index:0;amount:42;created
+	std::cout << "index:" << this->_accountIndex 
+			  << ";amount:" << this->_amount
+			  << ";created" << std::endl ;
+}
+
+Account::~Account(void)
+{
+    _displayTimestamp();
+    std::cout << "index:" << this->_accountIndex
+              << ";amount:" << this->_amount
+              << ";closed" << std::endl;
+}
+
+int Account::getNbAccounts(void) { return Account::_nbAccounts; }
+int Account::getTotalAmount(void) { return Account::_totalAmount; }
+int Account::getNbDeposits(void) { return Account::_totalNbDeposits; }
+int Account::getNbWithdrawals(void) { return Account::_totalNbWithdrawals; }
+
+void	Account::displayAccountsInfos( void )
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts()
+			  << ";total:" << getTotalAmount()
+			  << ";deposits" << getNbDeposits()
+			  << ";withdrawals:" << getNbWithdrawals()
+			  << std::endl ;
 }
